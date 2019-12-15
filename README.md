203f2af26d90c4915aa0946061312b02a8c9fa64
Douman <douman@gmx.se>
2019-12-15 09:50:44 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mehisha
+
+![](https://github.com/DoumanAsh/mehisha/workflows/Rust/badge.svg)
+
+![Mehisha image](https://raw.githubusercontent.com/DoumanAsh/mehisha/master/Mehisha.png)
+
+Rust Async runtime.
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+//! Mehisha is rust async runtime
+
+#![warn(missing_docs)]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/DoumanAsh/mehisha/master/Mehisha.png", html_favicon_url = "https://raw.githubusercontent.com/DoumanAsh/mehisha/master/Mehisha.png")]
+#![cfg_attr(feature = "cargo-clippy", allow(clippy::style))]
